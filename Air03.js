8f428dd577ed3c27159465ddc6048f16d20ef2bd
XO <xavierordonneau@gmail.com>
2023-04-24 15:28:07 +0200

    Ajout Air03.js

---
+++ Air03.js
@@ -0,0 +1,30 @@
+//node Air03.js "je" "teste" "des" "trucs" " " 
+const myConcat = (inputArray,inputSeparator) => {
+
+    let myString = ""
+    
+    for (let i = 0; i < inputArray.length; i++) {
+        if (i>0) {
+            myString += inputSeparator
+        }
+        myString+=inputArray[i]
+    }
+
+    return myString
+};
+
+const displayMyConcat = (inputArgs) => {
+
+    let listParam = process.argv.slice(2)
+    let separator = listParam.slice(-1) //dernier argument
+    let listofWords = listParam.slice(0,-1) //du 1er à l'avant dernier élément
+    if ( separator.length==0 || listofWords.length <= 1) {
+        console.log("Error")
+        return false
+    } else {
+        let mySentence = myConcat(listofWords, separator)
+        console.log(mySentence)
+    } 
+};
+
+displayMyConcat(process.argv.slice(2));
